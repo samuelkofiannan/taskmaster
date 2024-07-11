896cf98f30a0b5e5465b4a9adc2b196ee6dca60f
samuelkofiannan <qouphieligal@gmail.com>
2024-07-11 01:12:53 +0000

    taskmaster

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,21 +1,45 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Home from './pages/Home';
+import { Provider } from 'react-redux';
+import store from './redux/store';
+
+import Header from './components/Header';
+import Footer from './components/Footer';
+import Welcome from './pages/Welcome';
 import Login from './pages/Login';
 import SignUp from './pages/SignUp';
+import Home from './pages/Home';
+import AddTask from './pages/AddTask';
+import EditTask from './pages/EditTask';
 import Settings from './pages/Settings';
-import Welcome from './pages/Welcome';
 
-const App = () => (
-  <Router>
-    <Routes>
-      <Route exact path="/" component={Welcome} />
-      <Route path="/home" component={Home} />
-      <Route path="/login" component={Login} />
-      <Route path="/signup" component={SignUp} />
-      <Route path="/settings" component={Settings} />
-    </Routes>
-  </Router>
-);
+/**
+ * The main application component that sets up the routing and layout.
+ * 
+ * @component
+ */
+const App = () => {
+  return (
+    <Provider store={store}>
+      <Router>
+        <div className="App">
+          <Header />
+          <main>
+            <Routes>
+              <Route path="/" element={<Welcome />} />
+              <Route path="/login" element={<Login />} />
+              <Route path="/signup" element={<SignUp />} />
+              <Route path="/home" element={<Home />} />
+              <Route path="/add-task" element={<AddTask />} />
+              <Route path="/edit-task/:id" element={<EditTask />} />
+              <Route path="/settings" element={<Settings />} />
+            </Routes>
+          </main>
+          <Footer />
+        </div>
+      </Router>
+    </Provider>
+  );
+};
 
 export default App;
